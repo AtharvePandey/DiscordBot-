35a09659352d9ef122e90ce4cf403e064b44c274
Atharve Pandey <pandey.atharve@gmail.com>
2023-08-27 23:03:41 -0400

    logged in

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { BOT_TOKEN } from './secretVars';
-const {Client} = require('discord.js'); //returns an object of discord.js and gives reference to "Client" variable
-const client = new Client(); 
+const {Client, Message} = require('discord.js'); //returns an object of discord.js and gives reference to "Client" variable
+const ronBot = new Client(); 
 const token = BOT_TOKEN
 
-client.login(token); //connects to Discord, and my bot... 
+ronBot.login(token); //connects to Discord, and my bot...
+
+
