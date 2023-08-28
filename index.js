21fda9a42191d861c661d631a4329f039ba64a5c
Atharve Pandey <pandey.atharve@gmail.com>
2023-08-28 16:02:18 -0400

    done

--- index.js
+++ index.js
@@ -1,374 +1,125 @@
-// const { BOT_TOKEN } = require("./secretVars");
-// const { Client, Message, SlashCommandBuilder, GatewayIntentBits } = require("discord.js"); //returns an object of discord.js and gives reference to "Client" variable
-// const ronBot = new Client({
-//     intents: [GatewayIntentBits.MessageContent]
-// });
-// const token = BOT_TOKEN;
+const { BOT_TOKEN } = require("./secretVars");
+const { Client, GatewayIntentBits} = require("discord.js"); //returns an object of discord.js and gives reference to "Client" variable
+const ronBot = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages
+    ]
+});
+const token = BOT_TOKEN;
 
-// //json for the joke api
-// //[
-// // {
-// //     "error": boolean,
-// //     "category": string,
-// //     "type": string,
-// //     "setup": string,
-// //     "delivery": string,
-// //     "flags": {
-// //         "nsfw": boolean,
-// //         "religious": boolean,
-// //         "political": boolean,
-// //         "racist": boolean,
-// //         "sexist": boolean,
-// //         "explicit": boolean
-// //     },
-// //     "id": num,
-// //     "safe": boolean,
-// //     "lang": "en"
-// //  }
-// //]
+//json for the joke api
+//[
+// {
+//     "error": boolean,
+//     "category": string,
+//     "type": string,
+//     "setup": string,
+//     "delivery": string,
+//     "flags": {
+//         "nsfw": boolean,
+//         "religious": boolean,
+//         "political": boolean,
+//         "racist": boolean,
+//         "sexist": boolean,
+//         "explicit": boolean
+//     },
+//     "id": num,
+//     "safe": boolean,
+//     "lang": "en"
+//  }
+//]
 
-// //json for the fact api
-// // [
-// //     {
-// //       "fact": string
-// //     }
-// // ]
-
-// const greetingsArr = [
-//   "Hey there! Whats up ",
-//   "ew go away, i'm busy...doing other things...fine what do u want ",
-//   "I am Ron? what do u want ",
-//   "go bother Kal or Atharve nvm what do u want ",
-//   "what ",
-//   "whats up ",
-//   "ew its ",
-// ];
-
-// const commandsObj = {
-//   "joke": "insults whoever asked for it (20% chance), or makes an original joke fetched from the joke API",
-//   "kanye quote": "returns a random quote from kanye using the rest API",
-//   "fun fact": "returns a random fun fact",
-// };
-
-// ronBot.login(token); //connects to Discord, and my bot...
-
-// ronBot.on("ready", () => {
-//   //for debugging purposes...
-//   console.log("logged in, and connected to the API!");
-// });
-
-// ronBot.on("messageCreate", (message) => {
-//   if (message.author.bot) return;
-
-//   if (message.mentions.has(ronBot.user.id)) {
-//     //case where the bot was mentioned
-//     //generate a reply message with the possible commands given
-
-//     const greeting =
-//       greetingsArr[Math.floor(Math.random() * greetingsArr.length)] +
-//       message.author.id +
-//       "? \n";
-//     greeting = greeting.concat("Here is what I can do: " + "\n");
-//     greeting += commandsObj;
-
-//     message.reply(greeting).then((val) => {
-//       if (val) {
-//         //successfull reply
-//         ronBot.on("messageCreate", (message) => {
-//           if (message.content.includes("joke")) {
-//             //user has requested a joke
-//             //generate a random number between 1 and 5, so theres a 20% chance user gets insulted when asked for a joke
-//             let num = Math.floor(Math.random() * (5 - 1) + 1);
-//             if (num == 1) {
-//               //small chance the request invokes the bot to roast the user
-//               message.reply("you're the joke lol");
-//             } else {
-//               //now we call the api
-//               fetch("https://v2.jokeapi.dev/joke/Any").then((data) => {
-//                 data.json().then((json) => {
-//                   if (json.type == "twopart") {
-//                     const joke = json.setup + "\n" + json.delivery;
-//                     message.reply(joke);
-//                   } else {
-//                     message.reply(json.joke);
-//                   }
-//                 });
-//               });
-//             }
-//           } else if (message.content.includes("kanye")) {
-//             fetch("https://api.kanye.rest").then((val) => {
-//               //using the kanye rest api
-//               val.json().then((str) => {
-//                 message.reply("here is a quote by kanye: " + str.quote);
-//               });
-//             });
-//           } else if (message.content.includes("fact")) {
-//             fetch("https://api.api-ninjas.com/v1/facts?limit=1").then(
-//               (data) => {
-//                 data.json().then((json) => {
-//                   message.reply(json.fact);
-//                 });
-//               }
-//             );
-//           }
-//         });
-//       } else {
-//         message.reply(
-//           "Atharve made a small bug in the code, you shouldn't be reading this line at all, so someone tell him to quit being so bad at coding!"
-//         );
-//       }
-//     });
-//   }
-// });
-
-// const { BOT_TOKEN } = require("./secretVars");
-// const { Client, GatewayIntentBits } = require("discord.js");
-// const ronBot = new Client({
-//   intents: [GatewayIntentBits.MessageContent],
-// });
-// const token = BOT_TOKEN;
-
-// const greetingsArr = [
-//   "Hey there! What's up ",
-//   "Ew, go away, I'm busy... doing other things... fine, what do you want ",
-//   "I am Ron. What do you want ",
-//   "Go bother Kal or Atharve. Never mind, what do you want ",
-//   "What ",
-//   "What's up ",
-//   "Ew, it's ",
-// ];
-
-// ronBot.login(token);
-
-// ronBot.once("ready", () => {
-//   console.log("Logged in and connected to the API!");
-//   setBotPresence();
-// });
-
-// ronBot.on("messageCreate", handleMessage);
-
-// //chatGPT wrote setBotPresence()
-
-// function setBotPresence() {
-//   ronBot.user.setPresence({
-//     activities: [{ name: "ronBot", type: "LISTENING" }],
-//     status: "online",
-//   });
-// }
-
-// function handleMessage(message) {
-//   if (message.author.bot) return;
-
-//   if (message.mentions.has(ronBot.user.id)) {
-//     const command = message.content
-//       .slice(ronBot.user.id.length)
-//       .trim()
-//       .toLowerCase();
-
-//     switch (command) {
-//       case "joke":
-//         handleJokeCommand(message);
-//         break;
-//       case "kanye quote":
-//         handleKanyeQuoteCommand(message);
-//         break;
-//       case "fun fact":
-//         handleFunFactCommand(message);
-//         break;
-//       default:
-//         handleUnknownCommand(message);
-//         break;
+//json for the fact api
+// [
+//     {
+//       "fact": string
 //     }
-//   }
-// }
+// ]
 
-// function handleJokeCommand(message) {
-//   const num = Math.floor(Math.random() * 5) + 1;
+const greetingsArr = [
+  "Hey there! Whats up ",
+  "ew go away, i'm busy...doing other things...fine what do u want ",
+  "I am Ron? what do u want ",
+  "go bother Kal or Atharve nvm what do u want ",
+  "what ",
+  "whats up ",
+  "ew its ",
+];
 
-//   if (num === 1) {
-//     message.reply("You're the joke lol");
-//   } else {
-//     fetch("https://v2.jokeapi.dev/joke/Any")
-//       .then((response) => response.json())
-//       .then((json) => {
-//         const joke =
-//           json.type === "twopart" ? `${json.setup}\n${json.delivery}` : json.joke;
-//         message.reply(joke);
-//       })
-//       .catch(() => {
-//         message.reply("Failed to fetch a joke. Try again later.");
-//       });
-//   }
-// }
+const commandsObj = {
+  "joke": "insults whoever asked for it (20% chance), or makes an original joke fetched from the joke API",
+  "kanye quote": "returns a random quote from kanye using the rest API",
+  "fun fact": "returns a random fun fact",
+};
 
-// function handleKanyeQuoteCommand(message) {
-//   fetch("https://api.kanye.rest")
-//     .then((response) => response.json())
-//     .then((json) => {
-//       message.reply(`Here is a quote by Kanye West: "${json.quote}"`);
-//     })
-//     .catch(() => {
-//       message.reply("Failed to fetch a Kanye quote. Try again later.");
-//     });
-// }
+ronBot.login(token); //connects to Discord, and my bot...
 
-// function handleFunFactCommand(message) {
-//   fetch("https://api.api-ninjas.com/v1/facts?limit=1")
-//     .then((response) => response.json())
-//     .then((json) => {
-//       message.reply(json[0].fact);
-//     })
-//     .catch(() => {
-//       message.reply("Failed to fetch a fun fact. Try again later.");
-//     });
-// }
+ronBot.on("ready", () => {
+  //for debugging purposes...
+  console.log("logged in, and connected to the API!");
+});
 
-// function handleUnknownCommand(message) {
-//   message.reply(
-//     `Hey there! I'm not sure what you're asking. Here are the available commands:\n${formatCommands()}`
-//   );
-// }
+ronBot.on("messageCreate", (message) => {
+  if (message.author.bot) return;
 
-// //chatGPT wrote the formatCommands() method
+  if (message.mentions.has(ronBot.user.id)) {
+    //case where the bot was mentioned
+    //generate a reply message with the possible commands given
 
-// function formatCommands() {
-//   return Object.entries(commandsObj)
-//     .map(([command, description]) => `• **${command}**: ${description}`)
-//     .join("\n");
-// }
+    let greeting =
+      greetingsArr[Math.floor(Math.random() * greetingsArr.length)] +
+      message.author.displayName +
+      "? \n";
+    greeting = greeting.concat("Here is what I can do: " + "\n");
+    greeting += commandsObj;
 
-// const commandsObj = {
-//   "joke": "Get a random joke or a 20% chance of being playfully insulted.",
-//   "kanye quote": "Receive a random quote from Kanye West.",
-//   "fun fact": "Learn a fun fact to amaze your friends.",
-// };
-
-const fetchPromise = import("node-fetch");
-
-fetchPromise.then((module) => {
-  const fetch = module.default;
-
-  const { BOT_TOKEN } = require("./secretVars");
-  const { Client, GatewayIntentBits } = require("discord.js");
-  const ronBot = new Client({
-    intents: [GatewayIntentBits.MessageContent],
-  });
-  const token = BOT_TOKEN;
-
-  const greetingsArr = [
-    "Hey there! What's up ",
-    "Ew, go away, I'm busy... doing other things... fine, what do you want ",
-    "I am Ron. What do you want ",
-    "Go bother Kal or Atharve. Never mind, what do you want ",
-    "What ",
-    "What's up ",
-    "Ew, it's ",
-  ];
-
-  ronBot.login(token);
-
-  ronBot.once("ready", () => {
-    console.log("Logged in and connected to the API!");
-    setBotPresence();
-  });
-
-  ronBot.on("messageCreate", handleMessage);
-
-  //chatGPT wrote setBotPresence()
-
-  function setBotPresence() {
-    ronBot.user.setPresence({
-      activities: [{ name: "ronBot", type: "LISTENING" }],
-      status: "online",
+    message.reply(greeting).then((val) => {
+      if (val) {
+        //successfull reply
+        ronBot.on("messageCreate", (message) => {
+          if (message.content.includes("joke")) {
+            //user has requested a joke
+            //generate a random number between 1 and 5, so theres a 20% chance user gets insulted when asked for a joke
+            let num = Math.floor(Math.random() * (5 - 1) + 1);
+            if (num == 1) {
+              //small chance the request invokes the bot to roast the user
+              message.reply("you're the joke lol");
+            } else {
+              //now we call the api
+              fetch("https://v2.jokeapi.dev/joke/Any").then((data) => {
+                data.json().then((json) => {
+                  if (json.type == "twopart") {
+                    const joke = json.setup + "\n" + json.delivery;
+                    message.reply(joke);
+                  } else {
+                    message.reply(json.joke);
+                  }
+                });
+              });
+            }
+          } else if (message.content.includes("kanye")) {
+            fetch("https://api.kanye.rest").then((val) => {
+              //using the kanye rest api
+              val.json().then((str) => {
+                message.reply("here is a quote by kanye: " + str.quote);
+              });
+            });
+          } else if (message.content.includes("fact")) {
+            fetch("https://api.api-ninjas.com/v1/facts?limit=1").then(
+              (data) => {
+                data.json().then((json) => {
+                  message.reply(json.fact);
+                });
+              }
+            );
+          }
+        });
+      } else {
+        message.reply(
+          "Atharve made a small bug in the code, you shouldn't be reading this line at all, so someone tell him to quit being so bad at coding!"
+        );
+      }
     });
   }
-
-  function handleMessage(message) {
-    if (message.author.bot) return;
-
-    if (message.mentions.has(ronBot.user.id)) {
-      const command = message.content
-        .slice(ronBot.user.id.length)
-        .trim()
-        .toLowerCase();
-
-      switch (command) {
-        case "joke":
-          handleJokeCommand(message);
-          break;
-        case "kanye quote":
-          handleKanyeQuoteCommand(message);
-          break;
-        case "fun fact":
-          handleFunFactCommand(message);
-          break;
-        default:
-          handleUnknownCommand(message);
-          break;
-      }
-    }
-  }
-
-  function handleJokeCommand(message) {
-    const num = Math.floor(Math.random() * 5) + 1;
-
-    if (num === 1) {
-      message.reply("You're the joke lol");
-    } else {
-      fetch("https://v2.jokeapi.dev/joke/Any")
-        .then((response) => response.json())
-        .then((json) => {
-          const joke =
-            json.type === "twopart"
-              ? `${json.setup}\n${json.delivery}`
-              : json.joke;
-          message.reply(joke);
-        })
-        .catch(() => {
-          message.reply("Failed to fetch a joke. Try again later.");
-        });
-    }
-  }
-
-  function handleKanyeQuoteCommand(message) {
-    fetch("https://api.kanye.rest")
-      .then((response) => response.json())
-      .then((json) => {
-        message.reply(`Here is a quote by Kanye West: "${json.quote}"`);
-      })
-      .catch(() => {
-        message.reply("Failed to fetch a Kanye quote. Try again later.");
-      });
-  }
-
-  function handleFunFactCommand(message) {
-    fetch("https://api.api-ninjas.com/v1/facts?limit=1")
-      .then((response) => response.json())
-      .then((json) => {
-        message.reply(json[0].fact);
-      })
-      .catch(() => {
-        message.reply("Failed to fetch a fun fact. Try again later.");
-      });
-  }
-
-  function handleUnknownCommand(message) {
-    message.reply(
-      `Hey there! I'm not sure what you're asking. Here are the available commands:\n${formatCommands()}`
-    );
-  }
-
-  //chatGPT wrote the formatCommands() method
-
-  function formatCommands() {
-    return Object.entries(commandsObj)
-      .map(([command, description]) => `• **${command}**: ${description}`)
-      .join("\n");
-  }
-
-  const commandsObj = {
-    joke: "Get a random joke or a 20% chance of being playfully insulted.",
-    "kanye quote": "Receive a random quote from Kanye West.",
-    "fun fact": "Learn a fun fact to amaze your friends.",
-  };
 });
