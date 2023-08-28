62d0d3926aa13ac30e679777416849341a79cbd7
Atharve Pandey <pandey.atharve@gmail.com>
2023-08-28 10:05:08 -0400

    added an event listener for message ping, and then wrote logic for a few basic commands

--- index.js
+++ index.js
@@ -1,8 +1,61 @@
 import { BOT_TOKEN } from './secretVars';
-const {Client, Message} = require('discord.js'); //returns an object of discord.js and gives reference to "Client" variable
+const {Client, Message, SlashCommandBuilder} = require('discord.js'); //returns an object of discord.js and gives reference to "Client" variable
+const Spotify = require('spotify-web-api-js');
+const spotify = new Spotify(); 
 const ronBot = new Client(); 
 const token = BOT_TOKEN
 
+const greetingsArr = [
+    "Hey there! Whats up ",
+    "ew go away, i'm busy...doing other things...fine what do u want ",
+    "I am Ron? what do u want ",
+    "go bother Kal or Atharve, nvm what do u want ",
+    "what ",
+    "whats up ",
+    "ew its "
+]
+
+const commandsObj = {
+    "joke": "insults whoever asked for it",
+    "kanye quote": "returns a random quote from kanye using the rest API",
+    "song-reccomendations": "calls the song rec API and returns a random song"
+}
+
 ronBot.login(token); //connects to Discord, and my bot...
 
+ronBot.on('ready', () => { //for debugging purposes...
+    console.log("logged in, and connected to the API!"); 
+});
+
+ronBot.on('messageCreate', message =>{
+    if(message.author.bot)return;
+
+    if(message.mentions.has(ronBot.user.id)){ //case where the bot was mentioned
+        //generate a reply message with the possible commands given
+
+        const greeting = greetingsArr[Math.floor(Math.random() * greetingsArr.length)] + message.author.id + '? \n';
+        greeting = greeting.concat("Here is what I can do: " + '\n');
+        greeting += commandsObj;
+
+        message.reply(greeting).then(val => {
+            if(val){ //successfull reply
+                ronBot.on('messageCreate', message => {
+                    if(message.content.includes("joke")){ //user has requested a joke
+                        message.reply("you are the joke lol!");
+                    }else if(message.content.includes("kanye")){
+                        fetch("https://api.kanye.rest").then(val => {
+                            val.json().then(str => {
+                                message.reply("here is a quote by kanye: " + str.quote);
+                            })
+                        })
+                    }else if(message.content.includes("song")){
+
+                    }
+                })
+            }
+        })
+
+    }
+
+})
 
